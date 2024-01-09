9b7bc2fc5a94f49be9fd70d02438f09701ccdcf6
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2024-01-09 20:29:18 +0100

    Added the Example project

---
+++ Example/Program.cs
@@ -0,0 +1,9 @@
+﻿using UnmanagedCollections.Generic;
+
+var arr = new UnmanagedArray<byte>((long)uint.MaxValue + (long)1);
+
+arr.Fill(7);
+
+Console.WriteLine(arr[arr.Length - 1]);
+
+Console.ReadKey();
